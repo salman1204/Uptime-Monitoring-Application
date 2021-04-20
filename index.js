be4d519780bf56f0c3bf6cfd04c2749e99ec1973
Md Salman <md.salman@alphasq.com>
2021-04-20 19:49:40 +0600

    parsing http request url and handle request header and body

--- index.js
+++ index.js
@@ -7,6 +7,8 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // app object - module scaffolding
 const app = {};
@@ -26,7 +28,26 @@ app.createServer = () => {
 
 // handle request response
 app.handleReqRes = (req, res) => {
-    res.end('hello Progra');
+    // get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/|\/$/g, '');
+    const method = req.method.toLowerCase();
+    const queryStringObject = parsedUrl.query;
+    const headersObject = req.headers;
+
+    const decoder = new StringDecoder('utf-8');
+    let realData = '';
+
+    req.on('data', (buffer) => {
+        realData += decoder.write(buffer);
+    });
+
+    req.on('end', () => {
+        realData += decoder.end();
+        console.log(realData);
+        res.end('hello Programmers');
+    });
 };
 
 // start server
